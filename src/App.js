f1728efe4fb4dd8224af8e177dbecae3a01d6211
David Stefank <113402850+david-stefanik@users.noreply.github.com>
2023-04-23 13:11:39 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 
 
 function App() {
+  let [gethubName, setGithubName] = useState('')
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/learningToCode1234')
+      .then(res => res.json())
+      .then(data => {
+        setGithubName(data.name);
+      })
+  }, []);
+
   return (
     <div className="App">
       <h1>Github Profile Info:</h1>
+      <h2>{githubName}</h2>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,20 @@
+import { render,screen,waitFor } from "@testing-library/react";
+import App from './App';
 
+describe('when interacting with the git hub api',() => {
+   beforeEach(()=>{
+    fetch.resetMocks();
+   })
+   
+   
+    it('receives git hub name from ther rest api',async()=>{
+        fetch.mockResponseOnce(JSON.stringify({name:'coder'}));
+        render(<App/>);
+        let githubName = await waitFor(()=>{
+            return screen.getByRole('heading',{leval:2})
+        })
+        expect(githubName).toHaveTextContent('coder')
+    });
 
+});
 
